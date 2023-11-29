a87267aa64866bfbed977a7f50c1785368e27509
Artem <shevyakhov89@gmail.com>
2023-11-29 14:51:22 +0700

    feature/qrscanner token retrofit logic

--- libraries/network/src/main/java/com/chelz/network/NetworkModule.kt
+++ libraries/network/src/main/java/com/chelz/network/NetworkModule.kt
@@ -1,18 +1,34 @@
 package com.chelz.network
 
 import com.chelz.network.providers.provideOkHttpClient
+import com.chelz.network.providers.provideQrOkHttpClient
 import com.chelz.network.providers.provideRetrofit
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
-const val BACKEND = "BACKEND"
+const val BACKEND_QR = "BACKEND"
+const val BASIC_CLIENT = "BasicClient"
+const val QR_CLIENT = "QrClient"
+const val RETROFIT_BASIC = "RETROFIT_BASIC"
+const val RETROFIT_QR = "RETROFIT_QR"
+const val BACKEND_QR_KEY = "BACKEND_QR_KEY"
 val NetworkModule = module {
-	single {
+	single(named(BASIC_CLIENT)) {
 		provideOkHttpClient()
 	}
-	single {
+	single(named(QR_CLIENT)) {
+		provideQrOkHttpClient(getProperty(BACKEND_QR_KEY))
+	}
+	single(named(RETROFIT_BASIC)) {
 		provideRetrofit(
-			okHttpClient = get(),
-			url = getProperty(BACKEND)
+			okHttpClient = get(named(BASIC_CLIENT)),
+			url = getProperty(BACKEND_QR)
+		)
+	}
+	single(named(RETROFIT_QR)) {
+		provideRetrofit(
+			okHttpClient = get(named(QR_CLIENT)),
+			url = getProperty(BACKEND_QR)
 		)
 	}
 }
