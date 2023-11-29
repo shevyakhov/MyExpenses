69987649a3dc7bdc14d323309206c6debc54b1cf
Artem <shevyakhov89@gmail.com>
2023-11-29 14:53:48 +0700

    feature/qrscanner layer domain

---
+++ features/qrscanner/src/main/java/com/chelz/features/qrscanner/domain/entity/HtmlEntity.kt
@@ -0,0 +1,7 @@
+package com.chelz.features.qrscanner.domain.entity
+
+import java.io.Serializable
+
+data class HtmlEntity(
+	val html: String,
+) : Serializable
---
+++ features/qrscanner/src/main/java/com/chelz/features/qrscanner/domain/entity/ItemEntity.kt
@@ -0,0 +1,14 @@
+package com.chelz.features.qrscanner.domain.entity
+
+import java.io.Serializable
+
+data class ItemsEntity(
+	val list: List<SingleItemEntity>,
+) : Serializable
+
+data class SingleItemEntity(
+	var sum: Int? = null,
+	var name: String? = null,
+	var price: Double? = null,
+	var quantity: Int? = null,
+) : Serializable
